3c9acd292ce9a84381039f36160071b6f0ea6b10
tirto prayitno <tirtaiu@gmail.com>
2025-10-14 16:55:38 +0700

    update menu

--- app/Views/layout/menu.php
+++ app/Views/layout/menu.php
@@ -2,141 +2,33 @@
 <?php $uri = new \CodeIgniter\HTTP\URI(current_url(true)); ?>
 
 <li>
-    <a href="#dashboarrd" class="<?= $uri->getSegment(1) == "dashboard" ? "active" : null ?>">
-        <i data-cs-icon="dashboard-1" class="icon" data-cs-size="18"></i>
+    <a href="#dashboarrd" class="<?= isActive([1=>'dashboard']) ?>">
+        <i data-cs-icon="dashboard-1" class="icon" data-cs-size="15"></i>
         <span class="label">Dashboards</span>
     </a>
     <ul id="dashboarrd">
-        <!-- <li>
-            <a href="<?= site_url('dashboard/dashtd') ?>" class="<?= $uri->getSegment(2) == "dashtd" ? "active" : null ?>">
-                <span class="label">Old Dashboard</span>
-            </a>
-        </li> -->
-        <li>
-            <a href="<?= site_url('dashboard/ftkdash') ?>" class="<?= $uri->getSegment(2) == "ftkdash" ? "active" : null ?>">
-                <span class="label">Dashboard FTK</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('dashboard/ftkdash') ?>" class="<?= $uri->getSegment(2) == "ftkdash" ? "active" : null ?>">
-                <span class="label">Dashboard APS</span>
-            </a>
-        </li>
+        <?php
+            menuItem('Dashboard FTK', 'dashboard/ftkdash', 'role_mutasi', 'dashboard-1', [1=>'dashboard',2=>'ftkdash']);
+            menuItem('Dashboard APS', '#', 'role_mutasi', 'dashboard-1', [1=>'dashboard',2=>'aps']);
+            menuItem('Dashboard Mutasi', 'career/dashboardevalmutasi', 'role_mutasi', 'dashboard-1', [1=>'career',2=>'dashboardevalmutasi']);
+            menuItem('Dashboard Anggaran', 'anggaran', 'role_anggaran', 'dashboard-1', [1=>'anggaran',2=>null]);
+        ?>
     </ul>
 </li>
+
 <li>
-    <a href="#courses" class="<?= $uri->getSegment(1) == "strukturorg" ? "active" : null ?>">
-        <i data-cs-icon="building" class="icon" data-cs-size="18"></i>
+    <a href="#courses" class="<?= isActive([1=>'strukturorg']) ?>">
+        <i data-cs-icon="building" class="icon" data-cs-size="15"></i>
         <span class="label">FTK & Organisasi</span>
     </a>
     <ul id="courses">
-        <li>
-            <a href="<?= site_url('strukturorg') ?>">
-                <span class="label">Struktur Organisasi</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('strukturorg/monitoringftk') ?>" class="<?= $uri->getSegment(2) == "monitoringftk" ? "active" : null ?>">
-                <span class="label">Monitoring FTK</span>
-            </a>
-        </li>
-    </ul>
-</li>
-
-<li>
-    <a href="#instructors" class="<?= isActive([1=>'career']) ?>">
-        <i data-cs-icon="startup" class="icon" data-cs-size="15"></i>
-        <span class="label">Evaluasi Mutasi</span>
-    </a>
-    <ul id="instructors">
         <?php
-            menuItem('D-Evaluasi Mutasi', 'career/dashboardevalmutasi', 'role_mutasi', 'dashboard-1', [1=>'career',2=>'dashboardevalmutasi']);
-            menuItem('Draft Evaluasi', 'career/konsepeval', 'role_mutasi', 'book', [1=>'career',2=>'konsepeval']);
-            menuItem('Evaluasi Non APS', 'career/evaluasimutasi', 'role_mutasi', 'plane', [1=>'career',2=>'evaluasimutasi']);
-            menuItem('Evaluasi APS', 'career/evaluasimutasiaps', 'role_mutasi', 'flag', [1=>'career',2=>'evaluasimutasiaps']);
-            menuItem('Progess Evaluasi', 'career/telusurieval', 'role_mutasi', 'form-check', [1=>'pcs',2=>'telusurieval']);
-            menuItem('Evaluasi Selesai', 'career/selesaieval', 'role_mutasi', 'archive', [1=>'pcs',2=>'selesaieval']);
+            menuItem('Struktur Organisasi', 'strukturorg', 'role_organisasi', 'folders', [1=>'strukturorg',2=>null]);
+            menuItem('Monitoring FTK', 'strukturorg/monitoringftk', 'role_organisasi', 'toy', [1=>'strukturorg',2=>'monitoringftk']);
         ?>
-        <li>
-            <a href="#dokumenPrint" class="<?= isActive([2=>'lampeval']) ?>">
-                <i data-cs-icon="print" class="icon" data-cs-size="12"></i>
-                <span class="label">File to Print</span>
-            </a>
-            <ul id="dokumenPrint">
-                <?php
-                    menuItem('D-Evaluasi Mutasi', 'career/dashboardevalmutasi', 'role_mutasi', 'dashboard-1', [1=>'career',2=>'dashboardevalmutasi']);
-                    
-                ?>
-            </ul>
-        </li>
     </ul>
 </li>
 
-<li>
-    <?php $uri = new \CodeIgniter\HTTP\URI(current_url(true));
-    ?>
-    <a href="#instructors" class="<?= $uri->getSegment(1) == "career" ? "active" : null ?>">
-        <i data-cs-icon="startup" class="icon" data-cs-size="15"></i>
-        <span class="label">Evaluasi Mutasi</span><span class="position-absolute notification-dot rounded-xl"></span>
-    </a>
-    <ul id="instructors" class="">
-        
-        <li>
-            <a href="#interface" class="<?= $uri->getSegment(2) == "evaluasimutasi" ? "active" : null ?> <?= $uri->getSegment(2) == "evaluasimutasiaps" ? "active" : null ?>">
-                <i data-cs-icon="inbox" class="icon" data-cs-size="12"></i>
-                <span class="label">Kotak Masuk</span><span class="badge bg-danger text-small kotak-masuk"></span>
-            </a>
-            <ul id="interface">
-                <li>
-                    <a href="<?= site_url('career/evaluasimutasi') ?>" class="<?= $uri->getSegment(2) == "evaluasimutasi" ? "active" : null ?>">
-                        <span class="label">Non APS</span><span class="badge bg-danger text-small kotak-masuk-nonaps"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?= site_url('career/evaluasimutasiaps') ?>" class="<?= $uri->getSegment(2) == "evaluasimutasiaps" ? "active" : null ?>">
-                        <span class="label">APS</span><span class="badge bg-danger text-small kotak-masuk-aps"></span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= site_url('career/konsepeval') ?>" class="<?= $uri->getSegment(2) == "konsepeval" ? "active" : null ?>">
-                <i data-cs-icon="book" class="icon" data-cs-size="12"></i>
-                <span class="label">Konsep</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('career/telusurieval') ?>" class="<?= $uri->getSegment(2) == "telusurieval" ? "active" : null ?>">
-                <i data-cs-icon="search" class="icon" data-cs-size="12"></i>
-                <span class="label">Telusuri</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('career/lampeval') ?>" class="<?= $uri->getSegment(2) == "lampeval" ? "active" : null ?>">
-                <i data-cs-icon="file-text" class="icon" data-cs-size="12"></i>
-                <span class="label">Lampiran Evaluasi</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('career/bamutasi') ?>" class="<?= $uri->getSegment(2) == "bamutasi" ? "active" : null ?>">
-                <i data-cs-icon="file-text" class="icon" data-cs-size="12"></i>
-                <span class="label">Berita Acara Mutasi</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('career/reqkodpos') ?>" class="<?= $uri->getSegment(2) == "reqkodpos" ? "active" : null ?>">
-                <i data-cs-icon="file-text" class="icon" data-cs-size="12"></i>
-                <span class="label">Request Kode Posisi</span>
-            </a>
-        </li>
-        <li>
-            <a href="<?= site_url('career/selesaieval') ?>" class="<?= $uri->getSegment(2) == "selesaieval" ? "active" : null ?>">
-                <i data-cs-icon="check" class="icon" data-cs-size="12"></i>
-                <span class="label">Selesai</span>
-            </a>
-        </li>
-    </ul>
-</li>
 
 <li>
     <a href="#instructors1" class="<?= isActive([1=>'career']) ?>">
@@ -145,11 +37,39 @@
     </a>
     <ul id="instructors1">
         <?php
-            menuItem('Cari Kandidat', 'career', 'role_mutasi', 'search', [1=>'career',2=>null]);
-            menuItem('FnP Test', 'career/fnp', 'role_mutasi', 'circle', [1=>'career',2=>'fnp']);
-            menuItem('Interview', 'career/interview', 'role_mutasi', 'circle', [1=>'career',2=>'interview']);
-            menuItem('Gerbong Suksesi', 'pcs/gerbong', 'role_mutasi', 'circle', [1=>'pcs',2=>'gerbong']);
+            menuItem('Cari Kandidat', 'pcs', 'role_mutasi', 'search', [1=>'pcs',2=>null]);
+            menuItem('FnP Test', 'career/fnp', 'role_mutasi', 'quiz', [1=>'career',2=>'fnp']);
+            menuItem('Interview', 'career/interview', 'role_mutasi', 'mic', [1=>'career',2=>'interview']);
+            menuItem('Gerbong Suksesi', 'pcs/gerbong', 'role_mutasi', 'diagram-2', [1=>'pcs',2=>'gerbong']);
         ?>
+        <li>
+            <a href="#instructors" class="<?= isActive([1=>'career']) ?>">
+                <i data-cs-icon="startup" class="icon" data-cs-size="15"></i>
+                <span class="label">Evaluasi Mutasi</span>
+            </a>
+            <ul id="instructors">
+                <?php
+                    menuItem('Draft Evaluasi', 'career/konsepeval', 'role_mutasi', 'book', [1=>'career',2=>'konsepeval']);
+                    menuItem('Evaluasi Non APS', 'career/evaluasimutasi', 'role_mutasi', 'plane', [1=>'career',2=>'evaluasimutasi']);
+                    menuItem('Evaluasi APS', 'career/evaluasimutasiaps', 'role_mutasi', 'flag', [1=>'career',2=>'evaluasimutasiaps']);
+                    menuItem('Progess Evaluasi', 'career/telusurieval', 'role_mutasi', 'form-check', [1=>'pcs',2=>'telusurieval']);
+                    menuItem('Evaluasi Selesai', 'career/selesaieval', 'role_mutasi', 'archive', [1=>'pcs',2=>'selesaieval']);
+                ?>
+                <li>
+                    <a href="#dokumenPrint" class="<?= isActive([2=>'lampeval']) ?>">
+                        <i data-cs-icon="print" class="icon" data-cs-size="12"></i>
+                        <span class="label">File to Print</span>
+                    </a>
+                    <ul id="dokumenPrint">
+                        <?php
+                            menuItem('Lampiran Evaluasi', 'career/lampeval', 'role_mutasi', 'file-text', [1=>'career',2=>'lampeval']);
+                            menuItem('Berita Acara Mutasi', 'career/bamutasi', 'role_mutasi', 'file-text', [1=>'career',2=>'bamutasi']);
+                            menuItem('Request Kode Posisi', 'career/reqkodpos', 'role_mutasi', 'file-text', [1=>'career',2=>'reqkodpos']);
+                        ?>
+                    </ul>
+                </li>
+            </ul>
+        </li>
     </ul>
 </li>
 <!-- <li>
@@ -243,12 +163,11 @@
 </li> -->
 <li>
     <a href="#miscellaneous" class="<?= isActive([1=>'anggaran']) ?>">
-        <i data-cs-icon="wallet" class="icon" data-cs-size="15"></i>
+        <i data-cs-icon="dollar" class="icon" data-cs-size="15"></i>
         <span class="label">Anggaran</span>
     </a>
     <ul id="miscellaneous">
         <?php
-            menuItem('D-Anggaran', 'anggaran', 'role_anggaran', 'dashboard-1', [1=>'anggaran',2=>null]);
             menuItem('Pos 52', 'anggaran', 'role_adm_anggaran', 'leaf', [1=>'anggaran',2=>'pos52']);
             menuItem('Pos 53', 'anggaran/pos53', 'role_adm_anggaran', 'leaf', [1=>'anggaran',2=>'pos53']);
             menuItem('Pos 54', 'anggaran/pos54', 'role_adm_anggaran', 'leaf', [1=>'anggaran',2=>'pos54']);
